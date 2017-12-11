a37aee75da0a9a37921adb1be099c01b134648c2
Arun Alex <aalex.personal@gmail.com>
2017-12-11 21:40:32 +1100

    Login and signup

---
+++ src/app/app.firebase.config.ts
@@ -0,0 +1,8 @@
+export const FIREBASE_CONFIG = {
+    apiKey: "AIzaSyCFCDOSMtUlkxIhHd5Lj9_w_SlVHpPW34I",
+    authDomain: "test-app-359e9.firebaseapp.com",
+    databaseURL: "https://test-app-359e9.firebaseio.com",
+    projectId: "test-app-359e9",
+    storageBucket: "test-app-359e9.appspot.com",
+    messagingSenderId: "751377045694"
+  };
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,12 +1,15 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { LoginPage } from '../login/login';
+import { SignupPage } from '../signup/signup';
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
-
+  login = LoginPage;
+  signup = SignupPage;
   constructor(public navCtrl: NavController) {
 
   }
